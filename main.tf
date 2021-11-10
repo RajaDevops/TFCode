e972e1b2b4954101656b91b056376f0794c4ada8
Devops <devops@gmail.com>
2021-11-10 18:19:05 +0530

    mod var file

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
- 
-}
+    access_key = "${var.AWS_ACCESS_KEY}"
+    secret_key = "${var.AWS_SECRET_KEY}"
+    region = "${var.AWS_REGION}"
+ }
+
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {}
+
 
 variable "cidr_block" {
     description = "cidr blocks"
